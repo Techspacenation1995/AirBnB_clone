63860e51c8061aa62c5285781637808f12e9347a
aliola123 <alipoemoses6461@gmail.com>
2023-11-12 22:06:03 +0100

    updated the base_model to fit pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self):
         # Generate a unique identifier for the instance
@@ -14,7 +15,7 @@ class BaseModel:
 
     def __str__(self):
         # Return a string representation of the object
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         # Update the update timestamp to the current date and time
@@ -23,12 +24,7 @@ class BaseModel:
     def to_dict(self):
         # Create a copy of the instance's attributes dictionary
         obj_dict = self.__dict__.copy()
-        # Add class information to the dictionary
         obj_dict['__class__'] = self.__class__.__name__
-        # Convert the creation timestamp to ISO format and add it to the dictionary
         obj_dict['created_at'] = self.created_at.isoformat()
-        # Convert the update timestamp to ISO format and add it to the dictionary
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        # Return the dictionary representing the object
         return obj_dict
-
